1a22ac2ace088677f2f922f6c4da8fdb72b62573
ariss999 <87415367+ariss999@users.noreply.github.com>
2023-08-26 11:05:08 +0700

    update user data

--- react-app/src/app/userData.js
+++ react-app/src/app/userData.js
@@ -5,17 +5,62 @@ const userData = [
     email: "addmin@gmail.com",
     password: "123456789",
     profile: "",
-    cart: [],
-  },
-  {
-    id: 1,
-    name: "Hello word",
-    email: "abc@gmail.com",
-    password: "123456789",
-    profile: "",
     cart: [
       {
-        orderId: 1,
+        userID: 1,
+        orderId: 2,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 2,
+            quantity: 2,
+          },
+        ],
+        productStatus: "TO PAY",
+        totalPrice: 0,
+      },
+      {
+        userID: 1,
+        orderId: 3,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 1,
+            quantity: 1,
+          },
+        ],
+        transport:"J&T",
+        track:101010101000,
+        productStatus: "TO RECEIVE",
+        totalPrice: 600,
+      },
+      {
+        userID: 1,
+        orderId: 4,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 1,
+            quantity: 1,
+          },
+        ],
+        transport:"J&T",
+        track:101010101000,
+        productStatus: "TO RECEIVE",
+        totalPrice: 50,
+      },
+      {
+        userID: 1,
+        orderId: 5,
         order: [
           {
             productID: 1,
@@ -26,8 +71,18 @@ const userData = [
             quantity: 1,
           },
         ],
-        productStatus: "COMPLETED",
+        productStatus: "TO SHIP",
+        totalPrice: 50,
       },
+    ],
+  },
+  {
+    id: 1,
+    name: "Hello word",
+    email: "abc@gmail.com",
+    password: "123456789",
+    profile: "",
+    cart: [
       {
         orderId: 2,
         order: [
@@ -35,8 +90,62 @@ const userData = [
             productID: 1,
             quantity: 1,
           },
+          {
+            productID: 2,
+            quantity: 2,
+          },
         ],
         productStatus: "TO PAY",
+        totalPrice: 0,
+      },
+      {
+        orderId: 3,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 1,
+            quantity: 1,
+          },
+        ],
+        transport:"J&T",
+        track:101010101000,
+        productStatus: "TO RECEIVE",
+        totalPrice: 600,
+      },
+      {
+        orderId: 4,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 1,
+            quantity: 1,
+          },
+        ],
+        transport:"J&T",
+        track:101010101000,
+        productStatus: "TO RECEIVE",
+        totalPrice: 50,
+      },
+      {
+        orderId: 5,
+        order: [
+          {
+            productID: 1,
+            quantity: 1,
+          },
+          {
+            productID: 2,
+            quantity: 1,
+          },
+        ],
+        productStatus: "TO SHIP",
+        totalPrice: 50,
       },
     ],
   },
